a60a6b27abc00f8f05dc28f739ce7ed3a20d1cf6
shekhulogy <shikhar.patel.96@gmail.com>
2025-02-21 21:13:05 +0530

    first commit

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -15,21 +15,36 @@ export const todoSlice = createSlice({
         isCheck: false,
       };
       state.todos.push(todo);
+
+      localStorage.setItem("myTodos", JSON.stringify(state.todos));
     },
     removeTodo: (state, action) => {
       state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+      localStorage.setItem("myTodos", JSON.stringify(state.todos));
     },
-    checkTodo: (state, action) => {
-      const selectedTodos = state.todos.filter(
-        (todo) => todo.id === action.payload
-      );
-      selectedTodos.map((todo) => {
-        todo.isCheck ? (todo.isCheck = false) : (todo.isCheck = true);
+    updateTodo: (state, action) => {
+      // const selectedTodos = state.todos.filter(
+      //   (todo) => todo.id === action.payload
+      // );
+
+      // console.log(state.todos[0]);
+      state.todos = state.todos.map((todo) => {
+        if (todo.id === action.payload) {
+          if (todo.isCheck === false) {
+            return { ...todo, isCheck: true };
+          } else {
+            return { ...todo, isCheck: false };
+          }
+        } else {
+          return todo;
+        }
+        console.log(state.todos);
       });
+      localStorage.setItem("myTodos", JSON.stringify(state.todos));
     },
   },
 });
 
-export const { addTodo, removeTodo, checkTodo } = todoSlice.actions;
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
